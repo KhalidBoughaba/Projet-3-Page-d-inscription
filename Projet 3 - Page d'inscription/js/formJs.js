6eb6f07ceb95cd7e2024c5178cb6f44679f213b5
KhalidBoughaba <76523347+KhalidBoughaba@users.noreply.github.com>
2021-03-01 09:38:03 +0100

    Projet 3 - Page d'inscription
    Page d'inscription (form)

---
+++ Projet 3 - Page d'inscription/js/formJs.js
@@ -0,0 +1,15 @@
+function om() {
+	
+	var name=document.getElementById("nom").value;
+	var Prénome=document.getElementById("pre").value;
+	var Adresse=document.getElementById("adr").value;
+	var Telephone=document.getElementById("tel").value;
+	var Email=document.getElementById("eml").value;
+	
+
+    document.getElementById("space").innerHTML+="Nom: "+name+"<br>";
+    document.getElementById("space").innerHTML+="prenom: "+Prénome+"<br>";
+    document.getElementById("space").innerHTML+="Adresse: "+Adresse+"<br>";
+	document.getElementById("space").innerHTML+="Telephone: "+Telephone+"<br>";
+    document.getElementById("space").innerHTML+="E-mail: "+Email+"<br>";
+}
